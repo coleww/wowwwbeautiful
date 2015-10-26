40d5a9dd953a7ae4d0bc73e6b129ec199174ae59
Cole Willsea <coleww@gmail.com>
2015-10-26 08:53:30 -0700

    holy craps it works!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
 var face_detect = require('face-detect')
 var Canvas = require('canvas')
-var Image = Canvas.Image
 var quidprofollow = require('quidprofollow')
 var tipots = require('this-is-probably-ok-to-say')
+var request = require('request')
 var Twit = require('twit')
 var config = require('./config')
 var T = new Twit(config)
@@ -23,39 +23,51 @@ quidprofollow({twitterAPIKeys: config}, function reportResults(err, followed, un
 
 
       // TODO maybe pop off tweets on a timer so as to avoid rate limits...
-      data.forEach(function(tweet){
-        console.log(tweet.text)
+      var inty
+
+      setInterval(function () {
+
+        var tweet = data.pop()
+        if (!tweet) process.exit(0)
+        console.log('pop pop', tweet.text)
 
         // reject tweets that don't have 1 image, or contain awful language, or that are retweets
         if (hasImage(tweet) && tipots(tweet.text) && !tweet.retweeted_status) {
           replyIfTheTweetIsASelfie(tweet)
+        } else {
+          console.log("BORK")
         }
-      })
+        if (!data.length) clearInterval(inty)
+      }, 15000)
     })
   })
 })
 
 function hasImage (tweet) {
+  // console.log(tweet)
   // for now, only accepting tweets that have exactly 1 image
-  return tweet.extended_entities.media && tweet.extended_entities.media.length == 1
+  return tweet.extended_entities && tweet.extended_entities.media && tweet.extended_entities.media.length == 1
   // TODO figure out how to handle multi-image tweets (accept them if they are all selfies?)
 }
 
 function replyIfTheTweetIsASelfie (tweet) {
-  img = new Image
 
+  var Image = Canvas.Image
+  img = new Image
+  console.log("LOADING", tweet.extended_entities.media[0].media_url)
   img.onload = function() {
     var width = img.width
     var height = img.height
     var canvas = new Canvas(width, height)
     var ctx = canvas.getContext('2d')
+    console.log("DRAWING", width, height)
     ctx.drawImage(img, 0, 0, width, height)
     var result = face_detect.detect_objects({ "canvas" : canvas,
       "interval" : 5,
       "min_neighbors" : 1 })
 
-    console.log('Found ' + result.length  + ' faces.')
-
+    console.log('Found ' + result.length  + ' faces in' + tweet.text)
+    console.log(result)
     // TODO figure out what to do with multiple faces. ugh geometry :<
     if (result.length == 1) {
       console.log(result[0])
@@ -67,9 +79,27 @@ function replyIfTheTweetIsASelfie (tweet) {
       // T.post('statuses/update', {status: res, in_reply_to_status_id: tweet.id_str}, function (err, data, response) {
       //   if (err) throw err
       //   console.log(data)
+      //   fs.unlink('./temp/' +tweet.extended_entities.media[0].media_url + '.png', function(){console.log('deleted something')}) // delete the temp selfie
       // })
     }
   }
 
-  img.src = tweet.extended_entities.media[0].media_url
+  console.log("WRITING")
+  var ws = fs.createWriteStream('./temp/' + tweet.extended_entities.media[0].media_url.replace(/\/|\:/g, '')) // temporarily save the selfie cuz idk how to write directly to the canvas :<
+
+  request(tweet.extended_entities.media[0].media_url).pipe(ws)
+
+  ws.on("finish", function(){
+    console.log("READING")
+    fs.readFile('./temp/' +tweet.extended_entities.media[0].media_url.replace(/\/|\:/g, ''), function(err, data){
+      if (err) throw err
+      console.log("READ")
+      img.src = data
+    })
+  })
 }
+
+
+setTimeout(function () {
+
+}, 100000000)
---
+++ wipe.js
@@ -0,0 +1,9 @@
+// run this to delete tempfiles that for some reason go undeleted sometimes
+var rmrf = require('rimraf')
+var fs = require('fs')
+rmrf('./temp', function (e) {
+  console.log(e)
+  fs.mkdir('./temp', function (e) {
+    console.log(e)
+  })
+})
