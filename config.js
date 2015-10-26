b68cdc40ff8bda5768976f650a8b2961065b4e91
Cole Willsea <coleww@gmail.com>
2015-10-26 09:07:50 -0700

    WOW COOL GOODBYE API KEYS

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "consumer_key": "zePjuC6O7SO8mBL2Mv7H11UI0",
-  "consumer_secret": "qZbm1BGToFYqsIYZqXHQimukLySbrgfCuDaAb89cTUpUXFgd9u",
-  "access_token": "4019149154-wWyGbr7Ez8Q6gqqJf5eX6VakIsHALpiyRKcoUfL",
-  "access_token_secret": "l1StSMhAN9CMHnsfPUgSz0M19wvdZjI2mlPIWdDOPGcGR"
-}
