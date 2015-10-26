9c77c2a0eceffc6ad79f5dfb186fefc0ad5389f8
Cole Willsea <coleww@gmail.com>
2015-10-26 06:31:07 -0700

    add watchful protector copy pasta

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
-module.exports = function (str) {
-  return 'hello ' + str
+var Twit = require('twit')
+var fs = require('fs')
+var quidprofollow = require('quidprofollow');
+var tipots = require('this-is-probably-ok-to-say')
+var T = new Twit(config)
+
+var name = 'wowwwbeautiful'
+var config = require('./config')
+
+quidprofollow({twitterAPIKeys: config}, function reportResults(err, followed, unfollowed) {
+  if (err) throw err
+  console.log('Followed:', followed);
+  console.log('Unfollowed:', unfollowed);
+  
+ 
+  T.get('statuses/user_timeline', {count: 1, screen_name: name, exclude_replies: false}, function (err, data, response) {
+    if (err) throw err
+    console.log('grabbing timeline since:', data[0].id_str)
+    
+    
+    T.get('statuses/home_timeline', {count: 200, since_id: data[0].id_str, exclude_replies: true}, function (err, data, response) {
+      if (err) throw err
+      console.log('got', data.length, 'tweets')
+      
+      
+      // maybe pop off tweets on a timer so as to avoid rate limits...
+      data.forEach(function(tweet){          
+        console.log(tweet.text)
+        
+        if (hasImage(tweet) && isSelfie(tweet) && tipots(tweet.text) && !tweet.retweeted_status) {
+        
+        // do stuff!!!!!!!!!!!!!!!!!!!!!        
+        // if (res) {
+        //   console.log('bingo', res)
+        //   T.post('statuses/update', {status: res, in_reply_to_status_id: tweet.id_str}, function (err, data, response) {
+        //     if (err) throw err
+        //     console.log(data)
+        //   })
+        }
+      
+      })
+      
+      
+    })
+  })
+})
+
+
+
+
+
+function hasImage (tweet) {
+  // ...
 }
+
+function isSelfie (tweet) {
+  // ...
+}
+
+
+
+
+
+
+
+/// face-detect boilerplate below:
+
+
+
+
 
 
 
