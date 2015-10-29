6218948a7a64d55a021fd2d73959539e1e3f9475
Cole Willsea <coleww@gmail.com>
2015-10-29 11:21:12 -0700

    write last id to a file in case bad tweets happen

--- index.js
+++ index.js
@@ -24,15 +24,12 @@ ids.push(1447613460) // never unfollow sui ever.
   if (err) throw err
   console.log('Followed:', followed)
   console.log('Unfollowed:', unfollowed)
-  
-  // grabbing the last tweet that was tweeted by the bot, so that way we don't double-reply to anyone. 
-  // we could probably just save the last id to a file or smth, but w/e
-  T.get('statuses/user_timeline', {count: 1, screen_name: name, exclude_replies: false}, function (err, data, response) {
-    if (err) throw err
-    console.log('grabbing timeline since:', data[0].id_str)
+  var last = fs.readFileSync('./lastId').toString()
+ 
+      console.log('grabbing timeline since:', last)
     // grabbing tweets since the last time we tweeted. IIRC this maxes out at 200. 
     // if the bot gets super popular i guess we can page through it. cool
-    T.get('statuses/home_timeline', {count: 200, since_id: data[0].id_str, exclude_replies: true}, function (err, data, response) {
+    T.get('statuses/home_timeline', {count: 200, since_id: last, exclude_replies: true}, function (err, data, response) {
       if (err) throw err
       console.log('got', data.length, 'tweets')
 
@@ -78,7 +75,7 @@ ids.push(1447613460) // never unfollow sui ever.
       }, 120000) // dont get rate limited
     })
   })
-})
+
 
 function hasImage (tweet) {
   // console.log(tweet)
@@ -136,7 +133,7 @@ function replyIfTheTweetIsASelfie (tweet) {
           T.post('statuses/update', {status: '@' + tweet.user.screen_name + ' ' + toot, in_reply_to_status_id: tweet.id_str}, function (err, data, response) {
            if (err) throw err
            console.log(data)
-
+           fs.writeFileSync('./lastId', data.id_str)
             // delete the temp selfie file.
            fs.unlink('./temp/' + tweet.extended_entities.media[0].media_url.replace(/\/|\:/g, ''), function(){console.log('deleted something')}) // delete the temp selfie
 
