8b8b18cc69d38a125eecf3174be1161fdf6a5c74
Cole Willsea <coleww@gmail.com>
2015-11-04 07:23:45 -0800

    add config example

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twitter: {
+    "consumer_key": "SPIDERS!",
+    "consumer_secret": "spiders!!!",
+    "access_token": "SPIDERS!",
+    "access_token_secret": "moreSPIDERS!"
+  },
+  filterRegex: /sayhername|tw |cw |trigger|warning|tw\:|cw\:/i,
+  selfieRegex: /selfie|selfiearmy|transisbeautiful|bodyposi|bodypositive|selfportrait/i
+}
