5e2d94afb8b52568ebe3b0adfe1179b1e6d4e5ca
Cole Willsea <coleww@gmail.com>
2015-11-15 08:47:43 -0800

    temporary

--- src/qpf.js
+++ src/qpf.js
@@ -1,10 +1,14 @@
 var quidprofollow = require('quidprofollow')
 var callNextTick = require('call-next-tick')
 var config = require('./config')
-quidprofollow({twitterAPIKeys: config.twitter, retainFilter: function (ids, done) {
-  ids.push(1447613460) // never unfollow sui ever.
-  callNextTick(done, null, ids)
-}}, function reportResults (err, followed, unfollowed) {
+quidprofollow({
+  twitterAPIKeys: config.twitter//, 
+  //retainFilter: function (ids, done) {
+//    ids.push(1447613460) // never unfollow sui ever.
+//    callNextTick(done, null, ids)/
+//  }
+  
+}, function reportResults (err, followed, unfollowed) {
   console.log('qpf-err:', err)
   if (err) throw err
   console.log('Followed:', followed)
