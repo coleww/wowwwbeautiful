0ab96dd0a12dd4e0d6ed70b8560206f171694678
Cole Willsea <coleww@gmail.com>
2016-03-17 10:22:51 -0700

    might fix tests? cool. other stuff to. alright. nice

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ var tweet = {
   extended_entities: {
     media: [
       {
-        url: 'www.example.png'
+        media_url: 'www.example.png'
       }
     ]
   }
@@ -45,7 +45,7 @@ tap.test('hasImage', function (t) {
   var manyImages = JSON.parse(JSON.stringify(tweet))
   manyImages.extended_entities.media.push({url: 'wowlol.png'})
   t.ok(!utils.hasImage(manyImages), 'only accepts single image tweets')
-  
+
   var gifImages = JSON.parse(JSON.stringify(tweet))
   manyImages.extended_entities.media.push({url: 'wowlol.gif'})
   t.ok(!utils.hasImage(manyImages), 'rejects gifs cuz y u taking a gif selfie thats a vine, yo!')
