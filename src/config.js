e97b5319415eb98e31a3549e66f9a3af9f090233
Cole Willsea <coleww@gmail.com>
2016-04-16 11:19:06 -0700

    improve the regex to avoid certain selfies

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
     'access_token': process.env.ACCESS_TOKEN,
     'access_token_secret': process.env.ACCESS_TOKEN_SECRET
   },
-  filterRegex: /sayhername|paris|porteouverte|france|pray|tw |cw |trigger|warning|tw\:|cw\:/i, // strings to not reply to
+  filterRegex: /\bsir|misgender|sayhername|paris|porteouverte|tdor|peace|france|pray|tw |cw |trigger|warning|tw\:|cw\:/i, // strings to not reply to
   selfieRegex: /selfie|selfiearmy|transisbeautiful|bodyposi|bodypositive|selfportrait/i, // strings to reply more often to
   ocrMax: 8, // max # of chars that can be detected in the image
   minSize: 10, // detected face should be greated than width / minSize
